01103d8f9056a601674fe0121c1c6cbfbdae9938
ellodave <34511073+ellorich@users.noreply.github.com>
2020-09-07 12:14:33 +0100

    Create cov-contrast.js

---
+++ accessibility/cov-contrast.js
@@ -0,0 +1,27 @@
+function covContrast_isRed() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("cov-contrast__dark--red");
+}
+function covContrast_isBlue() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("cov-contrast__dark--blue");
+}
+function covContrast_isGreen() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("cov-contrast__dark--green");
+}
+
+
+
+function covContrast_FontisWhite() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("cov-contrast__font--white");
+}
+function covContrast_FontisBlue() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("cov-contrast__font--blue");
+}
+function covContrast_FontisRed() {
+   var element = document.getElementById("myDIV");
+   element.classList.toggle("cov-contrast__font--red");
+}
